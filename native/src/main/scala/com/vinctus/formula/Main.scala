cd7eb3703d5b5678474ff41ad97e27d5b627c1f7
Edward A Maxedon Sr <edadma@gmail.com>
2022-09-24 18:23:12 -0400

    SC-479 native demo app

--- native/src/main/scala/com/vinctus/formula/Main.scala
+++ native/src/main/scala/com/vinctus/formula/Main.scala
@@ -1,12 +1,10 @@
 package com.vinctus.formula
 
 import scopt.OParser
-
 import java.io.File
 import scala.util.{Try, Using}
 
-type Action = "formula"
-case class Config(input: File, vars: Seq[(String, String)], actions: Seq[(Action, String)])
+case class Config(input: File, vars: Seq[(String, String)], formulae: Seq[String])
 val stringRegex = "'([^']*)'|\"([^\"]*)\"".r
 
 @main def run(args: String*): Unit =
@@ -22,7 +20,7 @@ val stringRegex = "'([^']*)'|\"([^\"]*)\"".r
       opt[String]('f', "formula")
         .unbounded()
         .valueName("<name>")
-        .action((x, c) => c.copy(actions = c.actions :+ ("formula", x)))
+        .action((x, c) => c.copy(formulae = c.formulae :+ x))
         .text("compute formula value"),
       opt[Seq[(String, String)]]('s', "set")
         .valueName("x=a,y=b...")
@@ -43,6 +41,6 @@ val stringRegex = "'([^']*)'|\"([^\"]*)\"".r
         case (name, n)                      => f.set(name, n.toDouble)
       }
 
-      actions foreach { case ("formula", name) => println(s"formula $name = ${f.formula(name)}") }
+      actions foreach { name => println(s"formula $name = ${f.formula(name)}") }
     case _ =>
   }
