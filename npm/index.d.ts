f8102dd809a1f145246a9623bba5aa82ad831100
Edward A Maxedon Sr <edadma@gmail.com>
2022-09-25 18:57:41 -0400

    SC-476 add get() to look up variables and constants

--- npm/index.d.ts
+++ npm/index.d.ts
@@ -2,5 +2,6 @@ export class Formulae {
     constructor(decls: string)
     formula(name: string): any
     func(name: string, ...args: any[]): any
+    get(name: string): any
     set(name: string, value: any): void
 }
